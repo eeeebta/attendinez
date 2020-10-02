69d4ab665ca9640ea1c8d7bd3a2d6252ddc472e3
eeeebta <eeeebta@gmail.com>
2020-10-02 13:35:39 -0400

    Added database functionality and verification functionality

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+from flask_socketio import SocketIO
+
 import secrets
+
+from datetime import datetime
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-engine = create_engine("sqlite:///db/attendinez.db")
-db = scoped_session(sessionmaker(bind=engine))
 
 from helper import *
 
@@ -44,6 +46,12 @@ def after_request(response):
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+# TODO since PyLinter kinda doesn't like SQLAlchemy I'm using SQLite for the time being
+# TODO probably don't forget to check for same thread
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
 Session(app)
 
 
@@ -74,7 +82,7 @@ def main():
             filename = secure_filename(file.filename)
 
             # Generate a key
-            key = key_gen()
+            key = key_gen(16)
 
             # Format the file to give it a new name
             formatted_file_name = f"{filename[0:len(filename) - 4]}_{key}_{filename[-4:]}"
@@ -84,6 +92,7 @@ def main():
 
             # TODO Turn class_id into a list of things as opposed to just this one thing and this can be done
             # through having the user have access to some sort of text field and separate the values through commas
+            # request.form.get("class_id") -- enter variations OR allow a submission of numerous
             return redirect(url_for("process_file", path=f"{formatted_file_name}", class_ids="t627a-001 t627a-001 "
                                                                                              "t627 t627a"))
         else:
@@ -104,7 +113,6 @@ def main():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-
     # Clear session
     session.clear()
 
@@ -131,7 +139,8 @@ def register():
         # I should validate the email somewhere here
 
         hashed_pass = generate_password_hash(p1)
-        db.execute("INSERT INTO users(username, password) VALUES (:username, :hash)", {"username": user_inp, "hash": hashed_pass})
+        db.execute("INSERT INTO users(username, password) VALUES (:username, :hash)",
+                   {"username": user_inp, "hash": hashed_pass})
 
         db.commit()
 
@@ -166,16 +175,23 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username", {"username": request.form.get("username")}).fetchall()
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          {"username": request.form.get("username")}).fetchall()
 
         # Ensure username exists and password is correct
 
         # TODO Can add something here to be like "or not rows[0]["verified"]
+        # if not rows[0][3]:
+        #    # TODO "click here to resend it"?
+        #    return apology("well, you should probably confirm this account. until then this account is inactive. you "
+        #                   "have received an email already.", 403)
         if len(rows) != 1 or not check_password_hash(rows[0][2], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0][0]
+        print(session["user_id"])
+        db.close()
 
         # Redirect user to home page
         return redirect("/")
@@ -208,6 +224,11 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 
+def handle_attendance_contents():
+    # TODO
+    return "TODO"
+
+
 # TODO Not optimal at all and instead I want to use user sessions/ids to store the data, or javascript cookies to store
 # the path temporarily before throwing it away after processing -- this way people can't just try to
 # guess files and classes, although it probably wouldn't matter all too much as it is only available for a certain time
@@ -220,8 +241,11 @@ def process_file(class_ids, path):
     classes = class_ids.split()
 
     # Open the file and read the lines
-    with open(path, "r") as f:
-        file_contents = f.readlines()
+    try:
+        with open(path, "r") as f:
+            file_contents = f.readlines()
+    except FileNotFoundError:
+        return apology("File not found, bucko. Don't try and guess the file names!", 404)
 
     # Make everything lower case
     file_contents = [message.lower() for message in file_contents]
@@ -264,10 +288,37 @@ def allowed_files(file_name):
     return "." in file_name and file_name.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@app.route("/verify/<key>")
+def verify_account(key):
+    is_key_found = db.execute("SELECT * FROM confirmations WHERE con_key=:con_key", {"con_key": key}).fetchone()
+
+    # Figure out date/time stuff here
+    request_dt = is_key_found[4]
+
+    # Check if the key has been used already and make sure the object exists
+    if is_key_found and not is_key_found[3]:
+        user_id = is_key_found[1]
+
+
+# Create a verification key and relevant information
+def create_verification():
+    key = key_gen(32)
+    user_id = session["user_id"]
+    request_date = get_current_formatted_datetime()
+    db.execute("INSERT INTO confirmations (user_id, con_key, request_date) VALUES (:user_id, :con_key, :request_date)",
+               {"user_id": user_id, "con_key": key, "request_date": request_date})
+    return key
+
+
+# hah, american formatting
+def get_current_formatted_datetime():
+    return datetime.now().strftime("%m-%d-%y %H:%M:%S")
+
+
 # Generate key for the file to make sure teachers or students cant access random files
-def key_gen():
-    return secrets.token_urlsafe(16)
+def key_gen(length):
+    return secrets.token_urlsafe(length)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True)
